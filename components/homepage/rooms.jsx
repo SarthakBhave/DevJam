323c83cca67379406a9ce85bf4e165085eb86b20
SarthakBhave <kanaiya2182@gmail.com>
2024-05-07 02:15:33 +0530

    prefinalupdate

--- components/homepage/rooms.jsx
+++ components/homepage/rooms.jsx
@@ -29,14 +29,14 @@ export default function RoomsDetails() {
      <section id="category">
         <div className="w-full py-12 bg-[#0c0b10] mt-20">
             <div className="w-full max-w-[90%] mx-auto">
-                <h1 className="text-4xl font-bold text-white text-center py-8 pb-16">CATEGORIES</h1>
+                <h1 className="text-4xl font-semibold text-white text-center py-8 pb-16">CATEGORIES</h1>
                 <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 gap-6 justify-evenly">
                     {Huddle01RoomData.map((room) => (
                         <div key={room.id} className="w-full flex flex-col bg-[#1e1e1e] rounded-lg">
-                            <Image src={room.ImageSRC} alt={room.roomName} width={1080} height={1080} className="p-4 aspect-video object-cover" />
+                            <Image src={room.ImageSRC} alt={room.roomName} width={1080} height={1080} className="p-4 aspect-video object-cover "  style={{borderRadius:"30px"}} />
                             <div className="p-6 pt-0 flex flex-col">
                                 <h2 className="text-white text-xl font-bold">{room.roomName}</h2>
-                                <p className="text-white text-sm">{room.roomDescription}</p>
+                                <p className="text-white text-md">{room.roomDescription}</p>
                                 <div className="flex justify-end pt-4">
                                     <button
                                         className="px-3 pt-1 border-white border-[1px] rounded-3xl w-32 h-8 flex justify-evenly" 
